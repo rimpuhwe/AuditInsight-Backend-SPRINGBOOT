2c9c325676a9e27a49f68ff20fb074cec7288b6f
Diana BUYINZA <buyinzadiana@gmail.com>
2026-03-25 20:03:26 +0200

    created service folder for auth and otp business logic

--- src/main/java/com/diana/auditinsightbackendspringboot/modules/auth/service/OtpService.java
+++ src/main/java/com/diana/auditinsightbackendspringboot/modules/auth/service/OtpService.java
@@ -1,5 +1,6 @@
 package com.diana.auditinsightbackendspringboot.modules.auth.service;
 
+import com.diana.auditinsightbackendspringboot.modules.auth.otp.OtpEntity;
 import com.diana.auditinsightbackendspringboot.modules.auth.otp.OtpRepository;
 import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
@@ -12,5 +13,14 @@ public class OtpService {
 
     public OtpService(OtpRepository otpRepository) {
         this.otpRepository = otpRepository;
+    }
+
+    //Generate OTP FOR A USER
+    public String generateOTP(Long userId){
+        String otp = String.format("%06d", new Random().nextInt(999999));
+        OtpEntity otpEntity = OtpEntity.builder()
+                .userId(userId)
+                .code(otp)
+
     }
 }
