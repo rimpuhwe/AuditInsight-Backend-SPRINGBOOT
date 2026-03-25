f47015590c22a8410d888f9e26e2d88032faf5f9
Diana BUYINZA <buyinzadiana@gmail.com>
2026-03-25 17:32:31 +0200

    created a user repository in entity for database interaction method.

---
+++ src/main/java/com/diana/auditinsightbackendspringboot/modules/auth/entity/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.diana.auditinsightbackendspringboot.modules.auth.entity.repository;
+
+import com.diana.auditinsightbackendspringboot.modules.auth.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
