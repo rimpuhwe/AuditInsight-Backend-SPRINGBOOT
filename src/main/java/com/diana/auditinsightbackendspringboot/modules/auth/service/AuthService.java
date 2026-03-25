1e51907d011637d42eff1f73049de4783b0af191
Diana BUYINZA <buyinzadiana@gmail.com>
2026-03-25 20:28:17 +0200

    created  auth service for entire authentication business logic

--- src/main/java/com/diana/auditinsightbackendspringboot/modules/auth/service/AuthService.java
+++ src/main/java/com/diana/auditinsightbackendspringboot/modules/auth/service/AuthService.java
@@ -1,5 +1,11 @@
 package com.diana.auditinsightbackendspringboot.modules.auth.service;
 
 import com.diana.auditinsightbackendspringboot.modules.auth.dto.*;
+import com.diana.auditinsightbackendspringboot.modules.auth.entity.User;
+import com.diana.auditinsightbackendspringboot.modules.auth.otp.OtpEntity;
+import com.diana.auditinsightbackendspringboot.modules.auth.entity.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthService {
 }
