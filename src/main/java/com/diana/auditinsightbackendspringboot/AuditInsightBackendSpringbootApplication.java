2b73bebc4ccc89f6e1b5ae30c24a882d102a205e
Diana BUYINZA <buyinzadiana@gmail.com>
2026-03-25 12:41:40 +0200

    Set up maven and pom.xml dependencies

---
+++ src/main/java/com/diana/auditinsightbackendspringboot/AuditInsightBackendSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.diana.auditinsightbackendspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditInsightBackendSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuditInsightBackendSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/diana/auditinsightbackendspringboot/AuditInsightBackendSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diana.auditinsightbackendspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditInsightBackendSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
