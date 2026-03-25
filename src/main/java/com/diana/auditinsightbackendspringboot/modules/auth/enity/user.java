5e9b2a38b2085b1f18c17f111c87fc661c8b278b
Diana BUYINZA <buyinzadiana@gmail.com>
2026-03-25 14:38:48 +0200

    just added annotations fro creating table and others

--- src/main/java/com/diana/auditinsightbackendspringboot/modules/auth/enity/user.java
+++ src/main/java/com/diana/auditinsightbackendspringboot/modules/auth/enity/user.java
@@ -3,6 +3,11 @@ package com.diana.auditinsightbackendspringboot.modules.auth.enity;
 import jakarta.persistence.*;
 import lombok.*;
 
-
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class user {
 }
