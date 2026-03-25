73b27255128bd4975b9a92bef60ca0c8737be206
Diana BUYINZA <buyinzadiana@gmail.com>
2026-03-25 16:07:43 +0200

    added annotations needed @Entity                          @Table(name = "otp_codes")                          @Data                          @NoArgsConstructor                          @AllArgsConstructor                          @Builder

--- src/main/java/com/diana/auditinsightbackendspringboot/modules/auth/otp/OtpEntity.java
+++ src/main/java/com/diana/auditinsightbackendspringboot/modules/auth/otp/OtpEntity.java
@@ -3,5 +3,12 @@ package com.diana.auditinsightbackendspringboot.modules.auth.otp;
 import jakarta.persistence.*;
 import lombok.*;
 import java.time.LocalDateTime;
-public class otpEntity {
+
+@Entity
+@Table(name = "otp_codes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OtpEntity {
 }
