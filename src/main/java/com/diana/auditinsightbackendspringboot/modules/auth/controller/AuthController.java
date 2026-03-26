f679e1a929176b61940cd9ed5a198c5599bbfed7
Diana BUYINZA <buyinzadiana@gmail.com>
2026-03-26 10:57:10 +0200

    created a security config

--- src/main/java/com/diana/auditinsightbackendspringboot/modules/auth/controller/AuthController.java
+++ src/main/java/com/diana/auditinsightbackendspringboot/modules/auth/controller/AuthController.java
@@ -2,7 +2,8 @@ package com.diana.auditinsightbackendspringboot.modules.auth.controller;
 
 import com.diana.auditinsightbackendspringboot.modules.auth.dto.*;
 import com.diana.auditinsightbackendspringboot.modules.auth.entity.User;
-
+import com.diana.auditinsightbackendspringboot.modules.auth.service.AuthService;
+import org.springframework.http.ResponseEntity;
 
 public class AuthController {
 }
