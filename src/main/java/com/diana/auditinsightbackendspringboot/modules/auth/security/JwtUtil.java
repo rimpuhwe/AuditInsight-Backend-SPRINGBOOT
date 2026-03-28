9189f05f55bcb79900fe4733ded8b041d55bc85b
Diana BUYINZA <buyinzadiana@gmail.com>
2026-03-28 22:23:00 +0200

    added JWT class

--- src/main/java/com/diana/auditinsightbackendspringboot/modules/auth/security/JwtUtil.java
+++ src/main/java/com/diana/auditinsightbackendspringboot/modules/auth/security/JwtUtil.java
@@ -1,6 +1,6 @@
 package com.diana.auditinsightbackendspringboot.modules.auth.security;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-
+import org.springframework.security.web.SecurityFilterChain;
 public class JwtUtil {
 }
