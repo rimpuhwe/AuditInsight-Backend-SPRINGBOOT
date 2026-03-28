71f6945136a097c5dc23efa9e8ce4ac0e61ff22d
Diana BUYINZA <buyinzadiana@gmail.com>
2026-03-28 23:30:07 +0200

    added JWT class

--- src/main/java/com/diana/auditinsightbackendspringboot/modules/auth/controller/AuthController.java
+++ src/main/java/com/diana/auditinsightbackendspringboot/modules/auth/controller/AuthController.java
@@ -12,7 +12,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
-    ...KKKNNNNNNNNNN
+    ...KKKNNNNNNNNNNSXXX
 
     private final AuthService authService;
 
