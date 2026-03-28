9711c9c15124447d6d1dba2aec467295b4a3d4f3
Diana BUYINZA <buyinzadiana@gmail.com>
2026-03-28 23:09:22 +0200

    added JWT class

--- src/main/java/com/diana/auditinsightbackendspringboot/modules/auth/security/JwtUtil.java
+++ src/main/java/com/diana/auditinsightbackendspringboot/modules/auth/security/JwtUtil.java
@@ -1,19 +1,17 @@
 package com.diana.auditinsightbackendspringboot.modules.auth.security;
 
 import io.jsonwebtoken.*;
-import org.springframework.*;
 import org.springframework.stereotype.Component;
-
 import java.util.Date;
 
 @Component
 public class JwtUtil {
 
-    private final String SECRET_KEY = "mySecretKey"; // keep this a secret
-    private final long EXPIRATION_MS = 1000 * 60 * 60;  // 1 hour
+    private final String SECRET_KEY = "mySecretKey"; // keep this secret!
+    private final long EXPIRATION_MS = 1000 * 60 * 60; // 1 hour
 
     // Generate token
-    public String generateToken(Long userId, String email){
+    public String generateToken(Long userId, String email) {
         return Jwts.builder()
                 .setSubject(String.valueOf(userId))
                 .claim("email", email)
@@ -23,11 +21,20 @@ public class JwtUtil {
                 .compact();
     }
 
-    // Validation token
-public boolean validateToken(String token){
+    // Validate token
+    public boolean validateToken(String token) {
         try {
-            Jwts.parser().setSigningKey(SECRET_KEY)
+            Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
 
-}
-
+    // Extract userId
+    public Long getUserIdFromToken(String token) {
+        Claims claims = Jwts.parser().setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token).getBody();
+        return Long.parseLong(claims.getSubject());
+    }
 }
