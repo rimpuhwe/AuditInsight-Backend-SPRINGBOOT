f3070a67e4a3be99968da49f815d13b837714459
Diana BUYINZA <buyinzadiana@gmail.com>
2026-04-15 14:38:58 +0200

    transactions repository

--- src/main/java/com/diana/auditinsightbackendspringboot/modules/transactions/repository/TransactionRepository.java
+++ src/main/java/com/diana/auditinsightbackendspringboot/modules/transactions/repository/TransactionRepository.java
@@ -1,4 +1,9 @@
 package com.diana.auditinsightbackendspringboot.modules.transactions.repository;
 
-public class TransactionRepository {
+import com.diana.auditinsightbackendspringboot.modules.transactions.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 }
