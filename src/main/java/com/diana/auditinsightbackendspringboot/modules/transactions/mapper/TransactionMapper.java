77ce2bc21ced0da60eef5ef7506df5f90b601103
Diana BUYINZA <buyinzadiana@gmail.com>
2026-04-15 15:31:35 +0200

    transactions mapper

--- src/main/java/com/diana/auditinsightbackendspringboot/modules/transactions/mapper/TransactionMapper.java
+++ src/main/java/com/diana/auditinsightbackendspringboot/modules/transactions/mapper/TransactionMapper.java
@@ -1,4 +1,32 @@
 package com.diana.auditinsightbackendspringboot.modules.transactions.mapper;
 
 public class TransactionMapper {
+
+    // DTO → ENTITY
+    public static Transaction toEntity(TransactionRequest dto) {
+        Transaction t = new Transaction();
+        t.setDate(dto.getDate());
+        t.setAmount(dto.getAmount());
+        t.setCounterparty(dto.getCounterparty());
+        t.setType(dto.getType());
+        t.setSource(dto.getSource());
+        t.setStatus(dto.getStatus());
+        t.setRiskScore(dto.getRiskScore());
+        return t;
+    }
+
+    // ENTITY → DTO
+    public static TransactionResponse toResponse(Transaction t) {
+        TransactionResponse dto = new TransactionResponse();
+        dto.setId(t.getId());
+        dto.setDate(t.getDate());
+        dto.setAmount(t.getAmount());
+        dto.setCounterparty(t.getCounterparty());
+        dto.setType(t.getType());
+        dto.setSource(t.getSource());
+        dto.setStatus(t.getStatus());
+        dto.setRiskScore(t.getRiskScore());
+        return dto;
+    }
+
 }
