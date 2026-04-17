2faba39dfcc9cdac50555c4d1a47008c958668bb
Diana BUYINZA <buyinzadiana@gmail.com>
2026-04-17 13:07:24 +0200

    changed the jwt

--- src/main/java/com/diana/auditinsightbackendspringboot/config/OAuth2SuccessHandler.java
+++ src/main/java/com/diana/auditinsightbackendspringboot/config/OAuth2SuccessHandler.java
@@ -1,22 +1,22 @@
-package com.diana.auditinsightbackendspringboot.config;
-
-import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.stereotype.Component;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-@Component
-public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
-
-    @Override
-    public void onAuthenticationSuccess(HttpServletRequest request,
-                                        HttpServletResponse response,
-                                        Authentication authentication)
-            throws IOException, ServletException {
-        // Redirect after successful login
-        response.sendRedirect("/dashboard");
-    }
-}
+//package com.diana.auditinsightbackendspringboot.config;
+//
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+//import org.springframework.stereotype.Component;
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//
+//@Component
+//public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
+//
+//    @Override
+//    public void onAuthenticationSuccess(HttpServletRequest request,
+//                                        HttpServletResponse response,
+//                                        Authentication authentication)
+//            throws IOException, ServletException {
+//        // Redirect after successful login
+//        response.sendRedirect("/dashboard");
+//    }
+//}
