ec9144c75a1a10fac5a1c8d4f3a339bcd117ba92
Diana BUYINZA <buyinzadiana@gmail.com>
2026-04-28 16:52:50 +0200

    EvidenceRequest dto

--- src/main/java/com/diana/auditinsightbackendspringboot/modules/evidence/dto/EvidenceResponse.java
+++ src/main/java/com/diana/auditinsightbackendspringboot/modules/evidence/dto/EvidenceResponse.java
@@ -1,4 +1,34 @@
 package com.diana.auditinsightbackendspringboot.modules.evidence.dto;
 
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
 public class EvidenceResponse {
+
+    private Long id;
+
+    private String name;
+
+    private String category;
+
+    private String subCategory;
+
+    private String type;
+
+    private String url;
+
+    private LocalDate date;
+
+    private String uploadedBy;
+
+    private LocalDateTime uploadedAt;
+
+    private String status;
+
+    private String notes;
+
+    private Long transactionId;
 }
