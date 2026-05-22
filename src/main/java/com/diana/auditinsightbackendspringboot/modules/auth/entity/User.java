1858a189e620834bac4fbc09febecbf09c14a360
Diana BUYINZA <buyinzadiana@gmail.com>
2026-05-22 08:40:25 +0200

    EvidenceMapper

--- src/main/java/com/diana/auditinsightbackendspringboot/modules/auth/entity/User.java
+++ src/main/java/com/diana/auditinsightbackendspringboot/modules/auth/entity/User.java
@@ -19,7 +19,7 @@ public class User {
     private String email;   // Email must be unique
 
     @Column(nullable = false)
-    private String password; // stores hashed passwords onlyy.
+    private String password; // stores hashed passwords only.
 
     @Column(nullable = false)
     private Boolean isVerified = false;  // to track OTP verification.
